506e934e575552d71187d4608b8a3fc1494ca3f2
Hu Xiaohong <xiaohong@pandas.run>
2023-05-12 18:32:55 +0800

    entrypoint script imporve

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# Setup Timezone
-if [ -v TZ ]; then
-	echo $TZ > /etc/timezone
-	export TZ
-fi
-
 # Create init config
 if [ ! -f /etc/ocserv/ocserv.conf ]; then
 	cat > ocserv.conf <<- EOCONF
@@ -26,13 +20,16 @@ if [ ! -f /etc/ocserv/ocserv.conf ]; then
 	max-same-clients = 0
 	try-mtu-discovery = true
 
+	# disable ssl3 tls1.0 tls1.1
+	tls-priorities = "NORMAL:%SERVER_PRECEDENCE:%COMPAT:-RSA:-VERS-SSL3.0:-VERS-TLS1.0:-VERS-TLS1.1"
+
 	device = vpns
 
-	ipv4-network = 192.168.99.0/24
+	ipv4-network = 192.168.100.0/24
 	ipv4-netmask = 255.255.255.0
 
 	route = default
-	route = 192.168.99.0/24
+	route = 192.168.0.0/24
 	no-route = 10.0.0.0/8
 	no-route = 100.64.0.0/10
 	no-route = 169.254.0.0/16
@@ -49,14 +46,17 @@ if [ ! -f /etc/ocserv/ocserv.conf ]; then
 
 	dns = 1.1.1.1
 	dns = 8.8.8.8
-	# dns = 223.5.5.5
 
 	# config file must as same as username or groupname
-	# config-per-user = /etc/ocserv/config-per-user/
-	# config-per-group = /etc/ocserv/config-per-group/
+	config-per-user = /etc/ocserv/config-per-user/
+	config-per-group = /etc/ocserv/config-per-group/
+	predictable-ips = true
 
 	# let ocserv to obtain information on the incoming session from loadbalancer
 	keepalive = 300
+
+	# uncomment if you use haproxy
+	# listen-proxy-proto = true
 
 	cisco-client-compat = true
 	ping-leases = false
@@ -71,7 +71,47 @@ fi
 # Create certificate
 if [ ! -f /etc/ocserv/server.cert ]; then
 
-	if [ -v EMAIL ] && [ -v DOMAIN ]; then
+	if [ -z DOMAIN ]; then
+
+		# Create self signed certificate
+		CN="vpn.example.com"
+		ORG="Organization"
+		DAYS=3650
+		IP=$(curl -s ipinfo.io/ip)
+
+		certtool --generate-privkey --outfile ca-key.pem
+		cat > ca.tmpl <<-EOCA
+		cn = "$CN"
+		organization = "$ORG"
+		serial = 1
+		expiration_days = $DAYS
+		ca
+		signing_key
+		cert_signing_key
+		crl_signing_key
+		EOCA
+		certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca.pem
+		certtool --generate-privkey --outfile server-key.pem
+		cat > server.tmpl <<-EOSRV
+		cn = "$CN"
+		organization = "$ORG"
+    serial = 2
+		expiration_days = $DAYS
+		signing_key
+		encryption_key
+		tls_www_server
+		# dns_name = "<your-hostname>"
+		ip_address = "$IP"
+		EOSRV
+		certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
+		echo "server-cert = /etc/ocserv/server-cert.pem" >> ocserv.conf
+		echo "server-key = /etc/ocserv/server-key.pem" >> ocserv.conf
+
+	else
+
+		if [ -z EMAIL ]; then
+			EMAIL="foo@example.com"
+		fi
 
 		# Create letsencrypt certificate
 		if [ -f /etc/ocserv/cloudflare.ini ]; then
@@ -97,54 +137,14 @@ if [ ! -f /etc/ocserv/server.cert ]; then
 		fi
 		service cron restart
 
-	else
-
-		# Create self signed certificate
-		CA_CN="vpn.example.com"
-		CA_ORG="CA_Organization"
-		CA_DAYS=999
-		SRV_CN="vpn.example.com"
-		SRV_ORG="My_Organization"
-		SRV_DAYS=999
-
-		if [ -v DOMAIN ]; then
-			CA_CN="$DOMAIN"
-			SRV_CN="$DOMAIN"
-		fi
-
-		certtool --generate-privkey --outfile ca-key.pem
-		cat > ca.tmpl <<-EOCA
-		cn = "$CA_CN"
-		organization = "$CA_ORG"
-		serial = 1
-		expiration_days = $CA_DAYS
-		ca
-		signing_key
-		cert_signing_key
-		crl_signing_key
-		EOCA
-		certtool --generate-self-signed --load-privkey ca-key.pem --template ca.tmpl --outfile ca.pem
-		certtool --generate-privkey --outfile server-key.pem
-		cat > server.tmpl <<-EOSRV
-		cn = "$SRV_CN"
-		organization = "$SRV_ORG"
-		expiration_days = $SRV_DAYS
-		signing_key
-		encryption_key
-		tls_www_server
-		EOSRV
-		certtool --generate-certificate --load-privkey server-key.pem --load-ca-certificate ca.pem --load-ca-privkey ca-key.pem --template server.tmpl --outfile server-cert.pem
-		echo "server-cert = /etc/ocserv/server-cert.pem" >> ocserv.conf
-		echo "server-key = /etc/ocserv/server-key.pem" >> ocserv.conf
-
 	fi
 
 fi
 
-# Create init user
+# Create init user for PAM authentication
 if [ ! -f /etc/ocserv/ocpasswd ]; then
 
-	if [ ! -v USERNAME ] && [ ! -v USERPASS ]; then
+	if [ -z USERNAME ] && [ -z USERPASS ]; then
 		# Create specific user
 		USERNAME='test'
 		USERPASS=$(openssl rand -base64 14)
@@ -156,18 +156,17 @@ if [ ! -f /etc/ocserv/ocpasswd ]; then
 	echo '----------------- User Generated ------------------'
 	echo "User: $USERNAME"
 	echo "Pass: $USERPASS"
-	echo '--------------------------------------------------------'
+	echo '---------------------------------------------------'
 
 fi
 
 # Open ipv4 ip forward
-#sysctl -w net.ipv4.ip_forward=1
-echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-sysctl -p
+sysctl -w net.ipv4.ip_forward=1
 
 # Enable NAT forwarding
-# iptables -t nat -A POSTROUTING -j MASQUERADE
-iptables -t nat -A POSTROUTING -s 192.168.99.0/24 -j SNAT --to-source $(hostname -I)
+# if you want to specific translate ip, uncomment the following line, -j MASQUERADE is dynamic way
+# iptables -t nat -A POSTROUTING -s 192.168.100.0/24 -j SNAT --to-source $(hostname -I)
+iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -p tcp --tcp-flags SYN,RST SYN -j TCPMSS --clamp-mss-to-pmtu
 
 # Enable TUN device
